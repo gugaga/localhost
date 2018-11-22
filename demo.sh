8a2538f38b2a4a10efe6a38a7770a45ccf861790
chenshuo <732174609@qq.com>
2018-11-22 22:41:21 +0800

    第三次提交

--- demo.sh
+++ demo.sh
@@ -5,8 +5,11 @@ else
    mkdir $1
    cd $1
    mkdir css js
+   touch index.html
    echo "<!DOCTYPE> <title>Hello</title> <h1>Hi</h1>" > index.html
+   touch style.css
    echo "h1{color: red;}" > css/style.css
+   touch main.js
    echo "var string = "Hello World " alert(string)" > js/main.js
    echo "success"
    exit
